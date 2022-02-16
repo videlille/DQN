d7ac301f1b3e14d2e36dda69aaff1e1c34500f4d
videlille <vtdl1993@hotmail.com>
2022-02-16 10:42:54 -0500

    Dueling and initialize_weights

--- Breakout/lib/atari_dqn_model.py
+++ Breakout/lib/atari_dqn_model.py
@@ -4,6 +4,17 @@ from torch.autograd import Variable
 
 import numpy as np
 
+def initialize_weights(m):
+  if isinstance(m, nn.Conv2d):
+      nn.init.kaiming_uniform_(m.weight.data,nonlinearity='relu')
+      if m.bias is not None:
+          nn.init.constant_(m.bias.data, 0)
+  elif isinstance(m, nn.BatchNorm2d):
+      nn.init.constant_(m.weight.data, 1)
+      nn.init.constant_(m.bias.data, 0)
+  elif isinstance(m, nn.Linear):
+      nn.init.kaiming_uniform_(m.weight.data)
+      nn.init.constant_(m.bias.data, 0)
 
 class DQN(nn.Module):
     def __init__(self, input_shape, n_actions):
