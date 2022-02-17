6b28f6243cd97bd70c50a010cddab2901f7243c4
videlille <vtdl1993@hotmail.com>
2022-02-17 05:09:56 -0500

    added PrioReplayBuffer

--- lib/atari_dqn_extra.py
+++ lib/atari_dqn_extra.py
@@ -93,6 +93,7 @@ class DuelingDQN(nn.Module):
         )
 
         conv_out_size = self._get_conv_out(input_shape)
+
         self.fc_adv = nn.Sequential(
             nn.Linear(conv_out_size, 256),
             nn.ReLU(),
@@ -116,3 +117,63 @@ class DuelingDQN(nn.Module):
         fx = x.float() / 256
         conv_out = self.conv(fx).view(fx.size()[0], -1)
         return self.fc_adv(conv_out), self.fc_val(conv_out)
+
+# replay buffer params
+BETA_START = 0.4
+BETA_FRAMES = 100000
+
+
+class PrioReplayBuffer:
+    def __init__(self, exp_source, buf_size, prob_alpha=0.6):
+        self.exp_source_iter = iter(exp_source)
+        self.prob_alpha = prob_alpha
+        self.capacity = buf_size
+        self.pos = 0
+        self.buffer = []
+        self.priorities = np.zeros(
+            (buf_size, ), dtype=np.float32)
+        self.beta = BETA_START
+
+    def update_beta(self, idx):
+        v = BETA_START + idx * (1.0 - BETA_START) / \
+            BETA_FRAMES
+        self.beta = min(1.0, v)
+        return self.beta
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def populate(self, count):
+        max_prio = self.priorities.max() if \
+            self.buffer else 1.0
+        for _ in range(count):
+            sample = next(self.exp_source_iter)
+            if len(self.buffer) < self.capacity:
+                self.buffer.append(sample)
+            else:
+                self.buffer[self.pos] = sample
+            self.priorities[self.pos] = max_prio
+            self.pos = (self.pos + 1) % self.capacity
+
+    def sample(self, batch_size):
+        if len(self.buffer) == self.capacity:
+            prios = self.priorities
+        else:
+            prios = self.priorities[:self.pos]
+        probs = prios ** self.prob_alpha
+
+        probs /= probs.sum()
+        indices = np.random.choice(len(self.buffer),
+                                   batch_size, p=probs)
+        samples = [self.buffer[idx] for idx in indices]
+        total = len(self.buffer)
+        weights = (total * probs[indices]) ** (-self.beta)
+        weights /= weights.max()
+        return samples, indices, \
+               np.array(weights, dtype=np.float32)
+
+    def update_priorities(self, batch_indices,
+                          batch_priorities):
+        for idx, prio in zip(batch_indices,
+                             batch_priorities):
+            self.priorities[idx] = prio
